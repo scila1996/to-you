4b53e89a1dbec041f6c03b2e4a10c4fe373b47c5
Nguyễn Trung <trungnt.hust.96@gmail.com>
2021-10-18 01:47:31 +0700

    First commit

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ new TypeIt('#type', {
   "Somewhere over the rainbow, blue birds fly",
   "And the dreams that you dream of, dreams really do come true..."],
 
-  breakLines: false,
-  loop: true,
+  breakLines: true,
+  loop: false,
   beforeStep: (step, queue, instance) => {
     instance.elements[0].style.color = randomColor();
   }
